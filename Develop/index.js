41a4e7665ca57e1de8b780dac1adc8822ed9f819
KSherazi <kecoston@gmail.com>
2020-09-09 16:24:15 -0500

    fs.writefile and readme layout success

--- Develop/index.js
+++ Develop/index.js
@@ -1,70 +1,88 @@
-var inquirer = require('inquirer');
-var fs = require("fs");
-var markdown = require('./utils/generateMarkdown')
-const { type } = require('os');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-// array of questions for user
-const questions = [
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
     {
-        message: "What is the title of your project?", 
-        type: "input",
-        name:"title"
-    }, 
-    {
-        message: "What does your application do?",
-        type: "input",
-        name: "description"
+      message: "What is the title of your project?",
+      type: "input",
+      name: "title"
     },
     {
-        message: "Are there any other programs or information that is needed to be installed for this application to function?",
-        type: "input",
-        name: "instillation"
+      message: "What does your application do?",
+      type: "input",
+      name: "description"
     },
     {
-        message: "Please provide a step by step guide on how a user would run this application.",
-        type: "input",
-        name: "userstory"
+      message: "Are there any other programs or information that is needed to be installed for this application to function?",
+      type: "input",
+      name: "instillation"
     },
     {
-        message: "Are there any contribution guidelines for this project?",
-        type: "input",
-        name: "contribution"
+      message: "Please provide a step by step guide on how a user would run this application.",
+      type: "input",
+      name: "userstory"
     },
     {
-        message: "Are there any test instructions for this application?",
-        type: "input",
-        name: "test"
+      message: "Are there any contribution guidelines for this project?",
+      type: "input",
+      name: "contribution"
+    },
+    {
+      message: "Are there any test instructions for this application?",
+      type: "input",
+      name: "test"
     }
-];
-
-inquirer
-  .prompt(
-    questions 
- )
-  .then(answers => {
-    markdown()
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      //Prompt couldn't be rendered in the current environment
-      console.log("Prompt couldn't be rendered in the current environment")
-    } else {
-      // Something else when wrong
-      console.log("Something went wrong")
-    }
-  });
-
-// function to write README file
-function writeToFile(fileName, data) {
-
-
-    
+  ]);
 }
 
-// function to initialize program
-function init() {
+function generateReadMe(answers) {
+  return `
+  ## Project: ${answers.title}
 
+  # Description: 
+  ${answers.description}
+  
+  # Table of Contents:
+  
+  * [Title](#Project)
+  * [Description](#Description)
+  * [Instillation](#Instillation) 
+  * [Usage](#User-Story)
+  * [License](#License)
+  * [Contributing](#Contributors)
+  * [Test](#Test-Instructions) 
+  * [Questions](#Questions)
+
+  # Instillation 
+  ${answers.instillation}
+
+  # User-Story 
+  ${answers.userstory}
+  
+  # Contributors 
+  ${answers.contribution}
+
+  # Test-Instructions 
+  ${answers.test}
+  `;
 }
 
-// function call to initialize program
+async function init() {
+  try {
+    const answers = await promptUser();
+
+    const readme = generateReadMe(answers);
+
+    await writeFileAsync("README.md", readme);
+
+    console.log("Successfully wrote to readme.md");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 init();
